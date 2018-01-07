f7c09d54d554082bcf57596a0b6aff77edbf5038
TAG School PTA <tagschoolpta@gmail.com>
2018-01-06 19:09:54 -0500

    update

--- cloudfunctions/workers/index.js
+++ cloudfunctions/workers/index.js
@@ -1,64 +1,27 @@
-
-const credentials = {
-  "name": "projects/tag-school-pta/locations/global/keyRings/master/cryptoKeys/secrets",
-  "ciphertext": "CiQA6894rxeOpmdknSmMt+Gn2RwexEYQJbvxDWwOkj2qRWJ0PmQSwAEALrBm8uyHgK4V1KohwELZkUAOCE4zeUh+6dpTv1D/49UgNH3iL7oi4VCEAi5zOheLYvJAf697o1hc2M0XtQA7TEWo+DBOzMsLovPpS0K0c4a9IaX09RRPPVcRElxwA6itUW3ZC8jDq431dQo4C5H4DzHQQsZhh7NrcufgS8FD0seA//jvxJu/caTaPQ5eD8frxo6uFlyOe81cu/U/Rs9axPOcV5tXQFaU0jp2stz0Gb0Z/BII17ieQ3JwP9nSYbE="
-}
-
-function buildAndAuthorizeService(callback) {
-  // Imports the Google APIs client library
-  const google = require('googleapis');
-  const cloudkms = google.cloudkms({
-    version: 'v1'
-  });
-
-  callback(null, cloudkms);
-  // // Acquires credentials
-  // google.auth.getApplicationDefault((err, authClient) => {
-  //   if (err) {
-  //     callback(err);
-  //     return;
-  //   }
-
-  //   if (authClient.createScopedRequired && authClient.createScopedRequired()) {
-  //     authClient = authClient.createScoped([
-  //       'https://www.googleapis.com/auth/cloud-platform'
-  //     ]);
-  //   }
-
-  //   // Instantiates an authorized client
-  //   const cloudkms = google.cloudkms({
-  //     version: 'v1',
-  //     auth: authClient
-  //   });
-
-  //   callback(null, cloudkms);
-  // });
-}
-
 /**
  * Responds to any HTTP request that can provide a "message" field in the body.
  *
  * @param {!Object} req Cloud Function request context.
  * @param {!Object} res Cloud Function response context.
  */
-exports.loadMailchimp = (req, res) => {
-  buildAndAuthorizeService((err, cloudkms) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send('Error receiving Cloud KMS');
-      return;
-    }
-  
-    const request = credentials;
+exports.syncMailchimp = (req, res) => {
+  if (req.method.toLowerCase() != 'post'){
+    res.status(400).send('Use POST method');
+    return;
+  }
+  var apikey = req.get('X-MC-KEY');
+  if (!apikey){
+    res.status(403).send('No MC Key specified!');
+    return;
+  }
 
-    // Encrypts the file using the specified crypto key
-    cloudkms.projects.locations.keyRings.cryptoKeys.decrypt(request, (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(400).send('Error Decrypting Credentials');
-        return;
-      }
-      res.status(200).send(result);
-    });
-  });
+var MailchimpApi = require('mailchimp-api-v3');
+var mailchimp = new MailchimpApi(api_key);
+mailchimp.
+
+
 };
+
+function save (data) {
+
+}
